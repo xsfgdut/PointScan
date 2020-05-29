3bc7a4f9318ab5dbb4c549b6b276b10b64a4002e
xiaoshifan <xsfgdutxk@126.com>
2020-05-29 15:43:58 +0800

    modify

--- app/src/main/java/com/nexwise/pointscan/activity/LoginActivity.java
+++ app/src/main/java/com/nexwise/pointscan/activity/LoginActivity.java
@@ -1,9 +1,6 @@
 package com.nexwise.pointscan.activity;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -12,10 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.nexwise.pointscan.R;
 import com.nexwise.pointscan.base.BaseAct;
@@ -29,9 +22,7 @@ import com.nexwise.pointscan.view.ServerConfigDialog;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import okhttp3.Request;
